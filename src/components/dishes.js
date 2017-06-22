9d59c7a870b12f45211ad0fb4e01174a73d98c30
jesse <jsoldat@hotmail.com>
2017-06-22 14:52:37 -0400

    can display names of dishes without underscores

--- src/components/dishes.js
+++ src/components/dishes.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchDishes} from '../actions';
+import map from 'lodash/map';
+import keys from 'lodash/keys';
+import filter from 'lodash/filter';
+import size from 'lodash/size';
 
 class Dishes extends Component {
 	componentWillMount() {
@@ -9,7 +13,17 @@ class Dishes extends Component {
 	renderDishes() {
 		const {dishes} = this.props;
 		console.log(dishes);
-
+		return map(dishes, (dish) => {
+			
+			let dishNames = keys(dish);
+			dishNames = map(dishNames, (name) => {
+				return name.replace(/_/g, ' ');
+			})
+					
+			return map(dishNames, (dishName, i) => {
+				return <li key={i}>{dishName}</li>
+			})
+		});
 	}
 
 	render() {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import reducers from './reducers';
 
 import Dishes from './components/dishes';
