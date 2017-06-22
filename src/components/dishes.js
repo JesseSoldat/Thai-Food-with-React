49d97ffd6cdccb173f8461e2001b9e5302c2b9d0
jesse <jsoldat@hotmail.com>
2017-06-22 15:14:56 -0400

    reducer only returns dishes that have more than five items

--- src/components/dishes.js
+++ src/components/dishes.js
@@ -5,6 +5,8 @@ import map from 'lodash/map';
 import keys from 'lodash/keys';
 import filter from 'lodash/filter';
 import size from 'lodash/size';
+import pick from 'lodash/pick';
+import identity from 'lodash/identity';
 
 class Dishes extends Component {
 	componentWillMount() {
@@ -12,14 +14,14 @@ class Dishes extends Component {
 	}
 	renderDishes() {
 		const {dishes} = this.props;
-		console.log(dishes);
+		// console.log(dishes);
 		return map(dishes, (dish) => {
-			
+
 			let dishNames = keys(dish);
 			dishNames = map(dishNames, (name) => {
 				return name.replace(/_/g, ' ');
-			})
-					
+			});
+
 			return map(dishNames, (dishName, i) => {
 				return <li key={i}>{dishName}</li>
 			})
--- src/reducers/food.reducer.js
+++ src/reducers/food.reducer.js
@@ -1,12 +1,23 @@
 // import initialState from '../data/all.json';
 import {FETCH_DISHES} from '../actions/types';
+import size from 'lodash/size'
+import map from 'lodash/map'
+
 let initialState = {};
 
 export default function(state = initialState, action) {
 	switch(action.type) {
 		case FETCH_DISHES:
-			console.log('FETCHING DISHES');
-			return { ...state, ...action.payload};
+
+			let dishGT5 = {};  
+			map(action.payload, (dish, i) => {
+				let s = size(dish);
+
+				if (s <= 5) return null;
+				dishGT5[i] = dish;
+			});
+			console.log(dishGT5);
+			return { ...state, ...dishGT5};
 		default: 
 			return state;
 	}
